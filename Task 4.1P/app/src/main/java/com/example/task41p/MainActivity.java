a12497560cb835095489102d38381141b1591b3e
Ethan Richardson <s219220203@deakin.edu.au>
2024-04-15 10:30:29 +1000

    Progress on 4.1P
    current progress on this task, adding buttons to switch between activities

---
+++ Task 4.1P/app/src/main/java/com/example/task41p/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.task41p;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button buttonCreate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        //Declares
+        buttonCreate =findViewById(R.id.button);
+
+        //Click on buttonCreate
+        buttonCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivty(new Intent)
+            }
+        });
+    }
+}
